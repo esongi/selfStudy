451d6c2882271eb22d2c56193fbc86c9a91f3dfa
esongi <bazel29@naver.com>
2018-09-28 12:11:01 +0900

    check

---
+++ javaStudy/src/javaStudy/Intro.java
@@ -0,0 +1,10 @@
+package javaStudy;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("처음");
+		System.out.println("추가");
+	}
+
+}
---
+++ javaStudy/src/javaStudy/Variable.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int num = 1;
+		String word = "연습";
+   
+		System.out.println("int형: " + num);
+		System.out.println("String형: " + word);
+		System.out.println("공백");
+	}
+
+}
