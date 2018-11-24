cf048dd6292363cd1823c906ae7d82a12767ea12
esongi <bazel29@naver.com>
2018-11-24 16:47:50 +0900

    day 5 : 짝수 홀수 구분.. 간단한 거는 삼항연산자!!

--- javaEtc/src/javaStudy/programmers/Solution5.java
+++ javaEtc/src/javaStudy/programmers/Solution5.java
@@ -3,18 +3,13 @@ package javaStudy.programmers;
 public class Solution5 {
 
 	public String solution(int num) {
-		String answer = "";
 
-		if (num % 2 == 0) {
-			answer = "짝수";
-		} else {
-			answer = "홀수";
-		}
-		return answer;
+		// 간단한 거는 3항 연산자을 이용해보자
+		return num % 2 == 0 ? "Even" : "Odd";
 	}
 
 	public static void main(String[] args) {
 		Solution5 solution5 = new Solution5();
-		System.out.println(solution5.solution(0));
+		System.out.println(solution5.solution(3));
 	}
 }
